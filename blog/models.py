e32fa557f5eb6ba8e9790931ca650ade49651b89
Steve Hansen <steven.e.hansen@gmail.com>
2018-11-29 20:23:31 -0800

    Completing blog posts.

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,14 @@ class Blog( models.Model ):
     title = models.CharField( max_length=255 )
     pub_date = models.DateTimeField()
 
+    def __str__( self ):
+        return self.title
+
+    def summary( self ):
+        summary = self.body[:200]
+        if ( len( self.body ) > 200 ):
+            summary += "..."
+        return summary
+    
+    def pub_date_pretty( self ):
+        return self.pub_date.strftime( '%b %e %Y' )
